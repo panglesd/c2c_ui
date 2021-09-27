c303cecd4727cfe0155bee193f6a8542a8395dd2
Paul-Elliot Anglès d'Auriac <peada@free.fr>
2021-09-27 06:57:32 +0200

    Added a button to print a document list

---
+++ src/views/documents/utils/PrintButton.vue
@@ -0,0 +1,15 @@
+<template>
+  <router-link :to="path" tag="button" class="button is-primary is-size-7-mobile" @click="exportCsv">
+    Print
+  </router-link>
+</template>
+
+<script>
+export default {
+    computed: {
+	path() {
+	    return '/print'+this.$route.fullPath;
+	}
+    },
+};
+</script>
